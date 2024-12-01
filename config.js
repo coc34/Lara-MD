3f83589664c48199496326ac719eb6d20904f0f7
coc34 <cocc3474@gmail.com>
2024-12-01 14:13:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "SqoHUJ6a#B-fltxDbOvR2Lvmf2gh3r1kBuGho4_1bXGYmw4XODMs", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
